129775ffeb7f5dce78f7475db45eeb38567c2696
Joacobrule <jbrunengo@mail.austral.edu.ar>
2025-04-09 16:21:58 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,10 @@
 def check_vowels():
+        Nombre = input()
+        print("Contiene a:", "a" in Nombre or "A" in Nombre)
+        print("Contiene e:", "e" in Nombre or "E" in Nombre)
+        print("Contiene i:", "i" in Nombre or "I" in Nombre)
+        print("Contiene o:", "o" in Nombre or "O" in Nombre)
+        print("Contiene u:", "u" in Nombre or "U" in Nombre)
     # Código a implementar utilizando input.
 
 
